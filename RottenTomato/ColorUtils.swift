5aacf7c32b1db8bd1b48eee899393e0c4c17cf81
Giao Tuan <giaotuan.dev@gmail.com>
2015-11-15 21:57:33 +0700

    First commit

---
+++ RottenTomato/ColorUtils.swift
@@ -0,0 +1,9 @@
+//
+//  ColorUtils.swift
+//  RottenTomato
+//
+//  Created by Giao Tuan on 11/13/15.
+//  Copyright © 2015 LiFish. All rights reserved.
+//
+
+import Foundation
---
+++ RottenTomato/MovieItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieItemCell.swift
+//  RottenTomato
+//
+//  Created by Giao Tuan on 11/11/15.
+//  Copyright © 2015 LiFish. All rights reserved.
+//
+
+import UIKit
+
+class MovieItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
